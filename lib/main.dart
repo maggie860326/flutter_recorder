4bd55c673f9866d3a23ae4056ca30bb52e59a863
maggie860326 <maggie860326@gmail.com>
2023-10-13 01:11:50 +0800

    add instruction and 6 recorder pages in PageView

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
-import 'SwipeTest.dart';
+import 'swipe_test.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,21 +13,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Change Page',
-        theme: ThemeData(
-          primarySwatch: Colors.green,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
+      title: 'Change Page',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('換頁Demo'),
         ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('換頁Demo'),
-          ),
-          body: _FirstPage(),
-        ),
-        //todo: 註冊頁面
-        routes: <String, WidgetBuilder>{
-          '/SwipeTest': (_) =>  SwipeTest(),
-        });
+        body: _FirstPage(),
+      ),
+      // //todo: 註冊頁面
+      // routes: <String, WidgetBuilder>{
+      // '/SwipeTest': (_) => SwipeTest(initialPage: 0,),
+      // }
+    );
   }
 }
 
@@ -42,12 +43,19 @@ class _FirstPage extends StatelessWidget {
           child: Column(
             children: <Widget>[
               Card(
+                // 繼續上次測驗卡片
                 clipBehavior: Clip.hardEdge,
                 child: InkWell(
                   splashColor: Colors.blue.withAlpha(30),
                   onTap: () {
                     debugPrint('Card tapped.');
-                    Navigator.pushNamed(context, "/SwipeTest");
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SwipeTest(
+                            initialPage: 3,
+                          ),
+                        ));
                   },
                   child: const SizedBox(
                     width: 300,
@@ -57,12 +65,19 @@ class _FirstPage extends StatelessWidget {
                 ),
               ),
               Card(
+                //開始新測驗卡片
                 clipBehavior: Clip.hardEdge,
                 child: InkWell(
                   splashColor: Colors.blue.withAlpha(30),
                   onTap: () {
                     debugPrint('Card tapped.');
-                    Navigator.pushNamed(context, "/SwipeTest");
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SwipeTest(
+                            initialPage: 0,
+                          ),
+                        ));
                   },
                   child: const SizedBox(
                     width: 300,
